8835ff5fd00b56c8e77e6baefeaeae07548ac4c2
kelykely <305258029@qq.com>
2017-11-16 20:40:01 +0800

    提交

--- soup.py
+++ soup.py
@@ -1,21 +1,28 @@
+
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
+
 import requests
 from bs4 import BeautifulSoup
-import sys
 
 
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3)\
+    AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'}
 
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'}
-res = requests.get('https://nba.hupu.com/')
+res = requests.get('https://nba.hupu.com/',headers)
 #print(res.encoding)
 res.encoding = 'utf-8'
 #print(res.text)
 
 soup = BeautifulSoup(res.content,"html.parser")
+
 #print(soup.text)\
 news = soup.select(".nba-news-list")
-for v in news:
-    print (v.text)
-    print(v["href"])
+# print(news[0].text)
+# print(news[0].href)
+# print(len(news[0]))
+for item in news[0]:
+    print(item)
+    print(item.next)
+    
