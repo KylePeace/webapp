602f06386a5f063d568f4e05e4d402b6e2184c4b
kelykely <305258029@qq.com>
2017-12-05 23:23:49 +0800

    mongodb

--- scraping/scraping.py
+++ scraping/scraping.py
@@ -322,9 +322,31 @@
 #     print(result)
 
 #csv
-import csv
-with open(r'C:\Users\hukai\Desktop\test.csv',"r",encoding="UTF-8") as csvfile:
-    csv_reader = csv.reader(csvfile)
-    for row in csv_reader:
-        print(row)
-        print(row[0])
+# import csv
+# with open(r'C:\Users\hukai\Desktop\test.csv',"r",encoding="UTF-8") as csvfile:
+#     csv_reader = csv.reader(csvfile)
+#     for row in csv_reader:
+#         print(row)
+#         print(row[0])
+
+
+#mongo数据库操作
+import requests
+import datetime
+from bs4 import BeautifulSoup
+from pymongo import MongoClient
+client = MongoClient("localhost",27017)
+db = client.blog_database
+collection = db.blog
+
+link = "http://www.santostang.com/"
+headers = {'User-Agent': "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.89 Safari/537.36"}
+r = requests.get(link,headers = headers)
+
+soup = BeautifulSoup(r.text,"lxml")
+title_list = soup.find_all("h1",class_ = "post-title")
+for eachone in title_list:
+    url = eachone.a["href"]
+    title = eachone.a.text.strip()
+    post = {"url":url,"title":title,"date":datetime.datetime.utcnow()}
+    collection.insert_one(post)
